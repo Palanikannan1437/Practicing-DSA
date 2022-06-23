14a6922c213c67089f87a3c2460aa20eb38a2174
Palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2022-06-23 20:07:11 +0530

    linked list basics completed

---
+++ LinkedList/LinkedListBasics.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *takeInput()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *takeInputFromHead()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            n->next = head;
+            head = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+int main()
+{
+    Node *head = takeInput();
+    printLinkedList(head);
+    return 0;
+}
